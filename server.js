b14fdc07b7df369cb78c7bd52b42a36ad8a523fc
Humberto Hernandez <hamburger45@gmail.com>
2020-01-30 21:10:10 -0800

    complete again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function validateToken(req, res, next) {
 	const apiToken = process.env.API_TOKEN;
 	const authToken = req.get('Authorization');
 
-	if (!authToken || authToken.split(' ')[1] === apiToken) {
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
 		return res.status(401).json({ error: 'Unauthorized request' });
 	}
 	next();
